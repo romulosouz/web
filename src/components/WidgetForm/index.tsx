8458f7bab134d5ff9d2c39739124ea35677ff5bb
AgoraSoftware <contato@agorasoftware.com.br>
2022-05-04 19:52:36 -0300

    Front-End application

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,70 @@
+import { CloseButton } from "../CloseButton";
+import { useState } from "react";
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideiaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        },
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideiaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        },
+    },
+    OTHER: {
+        title: 'Outros',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagem de um balão'
+        },
+    },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm(){
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+    function handleRestartFeedback() {
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartResquested={ handleRestartFeedback } />
+            ) : (
+                <>
+                    { !feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep 
+                            feedbackType={ feedbackType } 
+                            onFeedbackRestartRequested={ handleRestartFeedback }
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    ) }
+                </>
+            )}            
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥ por <a className="underline underline-offset-2" href="https://www.agorasoftware.com.br">AgoraSoftware</a>
+            </footer>
+
+        </div>
+    );
+}
